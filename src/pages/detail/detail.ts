dae33a53bf33e87b6eb828837e3fd04ec07b615f
zixor <noel.gonzalez.h@gmail.com>
2017-06-02 09:24:17 -0500

    Adding camera and choose file for the project

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { ImagePicker } from '@ionic-native/image-picker';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { ExpenseSqliteService } from '../../providers/expense.service.sqlite';
 import { Expense } from '../../app/expense.model';
@@ -12,12 +14,28 @@ export class Detail {
 
   private expense;
   private categories: string[];
+  private base64Image: string;
 
+  private options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  };
+
+  private optionsGallery = {
+    maximumImagesCount: 1,
+    width: 500,
+    height: 500,
+    quality: 75
+  };
 
   constructor(private navCtrl: NavController,
     private navParms: NavParams,
     private expenseService: ExpenseSqliteService,
-    private alertCtrl: AlertController
+    private alertCtrl: AlertController,
+    private camera: Camera,
+    private imagePicker: ImagePicker
   ) {
     this.categories = ['Food', 'Travel', 'Other'];
     this.expense = {
@@ -36,8 +54,8 @@ export class Detail {
   }
 
   setDate(date) {
-   // this.expense.date = moment(date).format("MMM Do YYYY");   
-   this.expense.date = moment(date).format("YYYY-MM-DD");  
+    // this.expense.date = moment(date).format("MMM Do YYYY");   
+    this.expense.date = moment(date).format("YYYY-MM-DD");
   }
 
 
@@ -74,6 +92,24 @@ export class Detail {
       ]
     });
     confirm.present();
+  }
+
+  takePicture() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  choosePicture() {
+    this.imagePicker.getPictures(this.optionsGallery).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+        console.log('Image URI: ' + results[i]);
+      }
+    }, (err) => { });
   }
 
 }
