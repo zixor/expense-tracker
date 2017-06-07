36b7065ed550dc26363cb2421fd472c25c5d13e3
Noel Antonio Gonzalez Higuita <noel.gonzalez@globant.com>
2017-06-07 16:19:50 -0500

    Updating budget functionality

--- src/pages/budget/budget.ts
+++ src/pages/budget/budget.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, ModalController, AlertController } from 'ionic-angular';
 import { Detail } from '../detail/detail';
 import { BudgetSqliteService } from '../../providers/budget.service.sqlite';
 import { CategorySqliteService } from '../../providers/category.service.sqlite';
 import { Login } from '../login/login';
 import { DatePicker } from 'ionic2-date-picker/ionic2-date-picker';
 import { BudgetModel } from '../../models/budget.model';
+import { CategoryModel } from '../../models/category.model';
+
+import { ModalCategory } from '../modal-category/modal-category';
 
 @Component({
   selector: 'page-budget',
@@ -13,18 +16,28 @@ import { BudgetModel } from '../../models/budget.model';
 })
 export class Budget {
 
+  private budget: BudgetModel;
+  private category: CategoryModel;
+
   constructor(private navCtrl: NavController,
     private budgetService: BudgetSqliteService,
     private categoryService: CategorySqliteService,
+    private modalCtl: ModalController,
     private alertCtrl: AlertController
   ) {
 
+    var date = new Date();
+    let lastDayCurrentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0, 23, 59, 59);
+    this.budget.initialDate = date.setDate(1).toString();
+    this.budget.finalDate = new Date(lastDayCurrentMonth).toString();
+    this.budget.amount = 0;
+    this.budget.category = "";
 
   }
 
 
   ionViewWillEnter() {
- 
+
 
   }
 
@@ -32,19 +45,33 @@ export class Budget {
     this.navCtrl.push(Detail, {
       expense: expense
     });
-  }
-
-  doRefresh(refresher) {
-   
-  }
+  } 
 
   onAddClick() {
     this.navCtrl.push(Detail);
   }
 
 
-  onTrash(budget:BudgetModel) {
-    console.log("onTrash");
+  onSave() {
+    console.log(this.budget);
+    if (this.budget.id) {
+      this.budgetService.update(this.budget);
+    } else {
+      this.budgetService.add(this.budget);
+    }
+    this.navCtrl.pop();
+  }
+
+  openModalCategory() {
+    const modal = this.modalCtl.create(ModalCategory);
+    modal.present();
+
+    modal.onDidDismiss(category => {
+      this.category = category;
+    });
+  }
+
+  onTrash(budget: BudgetModel) {
     let confirm = this.alertCtrl.create({
       title: 'Delete',
       message: `Are you sure you want to delete this budget ?`,
@@ -58,7 +85,7 @@ export class Budget {
           text: 'Confirm',
           handler: () => {
             this.budgetService.delete(budget);
-            
+
           }
         }
       ]
